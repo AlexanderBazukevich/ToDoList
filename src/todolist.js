9cc3ec35ad4bb4a7ddb8780c62eeb9a9c95d6d8a
DeLaut <54510364+DeLaut@users.noreply.github.com>
2020-05-12 18:44:53 +0300

    added: notes deleting feature

--- src/todolist.js
+++ src/todolist.js
@@ -1,65 +1,137 @@
 const addForm = document.querySelector('form');
 const addField = document.querySelector('.control__field');
 const list = document.querySelector('.list');
+let nodes = list.childNodes;
 let notesList = [];
 
 if (localStorage.getItem('todos')) {
     notesList = JSON.parse(localStorage.getItem('todos'));
+    showNotes('todos');
 }
-
-showNotes(notesList);
 
 addForm.addEventListener('submit', (event) => {
     event.stopPropagation();
     event.preventDefault();
-
-    if (addNote() == false) {
-        return false;
-    }
-
-    addNote();
-    clearNotes(list);
-    showNotes(notesList);
-    makeVisible();
-})
-
-function addNote() {
     let titleValue = addField.value;
 
     if (titleValue == '') {
         return false;
     }
-    notesList.push({title: titleValue, group: "First", date:""});
-    localStorage.setItem('todos', JSON.stringify(notesList));
-    notesList = JSON.parse(localStorage.getItem('todos'));
+
+    addNote(notesList, titleValue);
     addField.value = '';
+})
+
+list.addEventListener('click', (event) => {
+
+    let e = event.target;
+    let note = getParentNote(event);
+
+    if (note == false) {
+        return false;
+    }
+
+    let notes = list.querySelectorAll('.note');
+    let control = note.querySelector('.control');
+    let labels = note.querySelectorAll('.control__label');
+    let index = Array.prototype.indexOf.call(notes, note); //how it works
+
+    if (e == labels[labels.length - 1].firstChild) {
+        if (e.checked) {
+            let fragment = document.createDocumentFragment();
+            let check = createCheck('fas', 'fa-times-circle', 'red');
+            fragment.append(check);
+            control.prepend(fragment);
+        } else {
+            control.removeChild(control.firstChild);
+        }
+    }
+
+    if (e == labels[0].firstChild && labels.length > 1) {
+        deleteNode(notesList, list, index);
+    }
+})
+
+function getParentNote(event) {
+    let e = event.target;
+
+    if (e.classList == 'list') {
+        return false;
+    }
+
+    while (e.classList != 'note') {
+        e = e.parentNode;
+    }
+
+    return e;
 }
 
-function showNotes(data) {
-    let fragment = document.createDocumentFragment();
+function addNote(data, title) {
+    createNote(title);
+    makeVisible();
+
+    data.push({title: title, group: "First", date:""});
+    localStorage.setItem('todos', JSON.stringify(data));
+}
+
+function deleteNode(data, elem, index) {
+    data.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(data));
+    
+    let animation = elem.childNodes[index].animate([
+        { opacity: '1' }, 
+        { opacity: '0' }
+    ], 300);
+    animation.onfinish = () => {
+        elem.removeChild(elem.childNodes[index]);
+    }
+}
+
+function showNotes(name) {
+    data = JSON.parse(localStorage.getItem(name));
 
     data.forEach( (item) => {
-        console.log(item.title);
-        let i = document.createElement('i');
-        let input = document.createElement('input');
-        let label = document.createElement('label');
-        let span = document.createElement('span');
-        let div = document.createElement('div');
-        i.classList.add('fas', 'fa-check-circle');
-        input.classList.add('note__selector');
-        input.setAttribute("type", "checkbox");
-        label.classList.add('note__check');
-        span.classList.add('note__content');
-        div.classList.add('note');
-        span.textContent = item.title;
-        label.append(input);
-        label.append(i);
-        div.append(span);
-        div.append(label);
-        fragment.append(div);
+        createNote(item.title);
     })
+}
 
+function createNote(content) {
+    let fragment = document.createDocumentFragment();
+    let check = createCheck('fas', 'fa-check-circle', 'blue');
+    let control = document.createElement('div');
+    let note = document.createElement('div');
+    let span = document.createElement('span');
+
+    control.classList.add('control');
+    control.append(check);
+    span.classList.add('note__content');
+    span.textContent = content;
+    note.classList.add('note');
+    note.append(span);
+    note.append(control);
+    fragment.append(note);
     list.append(fragment);
+}
+
+function createCheck(a, b, color) {
+    let fragment = document.createDocumentFragment();
+    let i = document.createElement('i');
+    let span = document.createElement('span');
+    let input = document.createElement('input');
+    let label = document.createElement('label');
+
+    i.classList.add(a, b);
+    span.classList.add('control__icon', 'control__icon_' + `${color}`);
+    input.classList.add('control__field');
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("hidden", "true");
+    label.classList.add('control__label');
+    span.append(i);
+    label.append(input);
+    label.append(span);
+    fragment.append(label);
+
+    return fragment;
 }
 
 function clearNotes(element) {
